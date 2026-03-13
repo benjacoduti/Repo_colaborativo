8741db5a700ada2efd50587820ebeeb68b132d24
Ramiro Yoffe <ryoffe@udesa.edu.ar>
2026-03-13 16:22:39 -0300

    Se creo la funcion analizar_habitos

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -6,3 +6,26 @@ Created on Fri Mar 13 15:52:28 2026
 @author: ramiroyoffe
 """
 
+def analizar_habitos(lista) :
+    '''
+    Recibe una lista de habitos, cuenta cuantas veces aparece cada habito. Guarda el resultado en un diccionario y devuelve ese diccionario.    
+
+    Parameters
+    ----------
+    lista : Lista
+        Una lista con todos los habitos realizados en un dia
+
+    Returns
+    -------
+    frequencia : DIccionario
+        Key: Habito.
+        Value: Frequencia del habito
+
+    '''
+    frequencia = {}
+    for habito in lista :
+        if habito not in frequencia :
+            frequencia[habito] = 1
+        else :
+            frequencia[habito] += 1
+    return frequencia
